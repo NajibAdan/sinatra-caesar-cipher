72c31797dce699e7d0818fd3d47c9c102497e2d6
Najib Adan <najibadan1@gmail.com>
2018-12-24 12:12:04 +0300

    First commit

---
+++ cipher.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/cipher' do
+    phrase = params['phrase']
+    shift = params['shift'].to_i
+    cipher = caesar_cipher(phrase,shift)
+    erb :index, :locals =>{:cipher => cipher}
+end
+
+def caesar_cipher(phrase,shift=1) 
+    return "" if phrase.nil?
+    cipherWord = []
+    wordsArray = phrase.split
+    wordsArray.each do |word|
+        cipher = ''
+        word.each_byte do |letter|
+            i = 0
+            while i<shift
+                if letter == 90 || letter == 122
+                    letter -= 25
+                elsif (letter > 64 && letter<123) && !(letter>90 && letter<97)
+                    letter = letter + 1
+                end
+                i +=1
+            end
+            cipher += letter.chr()
+        end
+        cipherWord.push(cipher)
+    end
+    return cipherWord.join(' ')
+end
